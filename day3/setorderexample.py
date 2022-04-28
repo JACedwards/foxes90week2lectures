04dade9702482116f1c9f34b7d90bd179cfe4206
sdavitt <sdavt2@gmail.com>
2022-04-28 12:08:28 -0500

    thursday am

---
+++ day3/setorderexample.py
@@ -0,0 +1,9 @@
+# showing that sets are truly unordered
+names = {'Metheuselah Honeysuckle', 'Earnest Lambert Watkins', 'TT Showbiz', 'Bruton Gaster'}
+
+print(names)
+names.add('Dr. Mc"Tock')
+print(names)
+names.remove('TT Showbiz')
+print(names)
+
